f52e23b21af40d1c8ffe76de5311620b91479519
Guilherme V. <imluizguilara@hotmail.com>
2023-11-01 13:21:03 -0300

    feat: Add cookie to persevere User on initial register

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method !== 'POST') {
@@ -26,6 +27,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 			name,
 			username,
 		},
+	})
+
+	setCookie({ res }, '@ignitecall:userId', user.id, {
+		maxAge: 60 * 60 * 24 * 7, // 7 days
+		path: '/',
 	})
 
 	return res.status(201).json(user)
