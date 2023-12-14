6f66e1cc515cb26f53183a2851c4499ca684f2c9
Guilherme Viana <imluizguilara@hotmail.com>
2023-12-14 13:52:46 -0300

    style: create & style timepicker list for calendar

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,46 @@
 import { Calendar } from '@/components/Calendar';
-import { Container } from './styles';
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles';
 
 export function CalendarStep() {
+  const isDateSelected = true;
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>15:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>18:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>19:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>20:00h</TimePickerItem>
+            <TimePickerItem>21:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   );
 }
